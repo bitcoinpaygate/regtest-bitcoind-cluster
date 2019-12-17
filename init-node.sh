b63d6e5de679ab8e089ff5f31269d3b3b1a750b3
Wojciech Langiewicz <wojciech@wlangiewicz.com>
2019-12-17 10:29:02 +0100

    set minrelayfee=0

--- init-node.sh
+++ init-node.sh
@@ -7,7 +7,7 @@ export NODE_NAME="node"
 
 mkdir $NODE_NAME
 
-bitcoind -dns=0 -datadir=$NODE_NAME -rpcport=$RPC_PORT -port=$PORT -addnode=127.0.0.1:8444 -regtest=1 -printtoconsole -rpcallowip=0.0.0.0/0 -rpcpassword=password -rpcuser=user &
+bitcoind -dns=0 -datadir=$NODE_NAME -rpcport=$RPC_PORT -port=$PORT -addnode=127.0.0.1:8444 -regtest=1 -printtoconsole -rpcallowip=0.0.0.0/0 -rpcpassword=password -rpcuser=user -minrelaytxfee=0 &
 
 sleep 5
 
